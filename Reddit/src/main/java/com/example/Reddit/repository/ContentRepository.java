bc4731931d0b8aadaf80f0c73bf49dc1f2f1f635
siddharth.aga <siddharth.aga@BBMDVSIDDHARTHAGA.local>
2021-02-03 17:44:34 +0530

    Controllers, Auth Added

--- Reddit/src/main/java/com/example/Reddit/repository/ContentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Reddit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.Reddit.model.Content;
-
-public interface ContentRepository extends JpaRepository<Content, Long> {
-
-}
